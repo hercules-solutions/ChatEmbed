ba56bc2fa107f4d04dd987e5fb9d668caa77a5e0
hercules-solutions <info@hercules-solutions.de>
2024-07-13 00:10:31 +0200

    Commit after build Badge tsx.

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Hercules-Solutions </span>
+        <span> Hercules Solutions </span>
       </a>
     </span>
   );
